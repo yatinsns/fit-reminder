2719e2e9c68a7f7490ccbb5b8ffbb355104714b8
Yatin Sarbalia <yatinsns@gmail.com>
2016-07-28 17:47:03 +0530

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'sinatra', '1.1.0'
+
---
+++ web.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+
+MESSAGES = ["Why kill time when you can make it work for you. Give feedback today! #F.I.T",
+	    "Everyday is feedback day. Give feedback today! #F.I.T",
+	    "Feedback in time, helps the team shine. Give feedback today! #F.I.T",
+	    "Hungry for food? thirsty for water? Everyone is eager for feedback. Give feedback today! #F.I.T",
+	    "It is time to get F.I.T. Give feedback today! ",
+	    "Everyone likes filled feedback sheets. Give feedback today! #F.I.T",
+	    "You will never know till you ask. Ask for feedback today! #F.I.T",
+	    "Feedback clock is ticking. Share feedback today! #F.I.T",
+	    "Did you check your feedback sheet today? Read feedback today!  #F.I.T",
+	    "Did you give feedback lately? Give feedback today! #F.I.T",
+	    "Feedback is the breakfast of champions. Give feedback today! #F.I.T"]
+
+get '/' do
+  message = MESSAGES[rand(MESSAGES.length)]
+  `curl -X POST -d '{"text":"#{message}"}' -H "Content-Type:application/json;charset=UTF-8" https://api.flock.co/hooks/sendMessage/5bb8d8db-0d55-436b-aeb2-c151bb4b61db`
+end
+
