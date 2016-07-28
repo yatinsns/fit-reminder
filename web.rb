9b49c4c240a09c0a4544c01b895e57c6a19bcbd8
Yatin Sarbalia <yatinsns@gmail.com>
2016-07-28 17:51:00 +0530

    Test fixed message

--- web.rb
+++ web.rb
@@ -14,6 +14,7 @@ MESSAGES = ["Why kill time when you can make it work for you. Give feedback toda
 
 get '/' do
   message = MESSAGES[rand(MESSAGES.length)]
-  `curl -X POST -d '{"text":"#{message}"}' -H "Content-Type:application/json;charset=UTF-8" https://api.flock.co/hooks/sendMessage/5bb8d8db-0d55-436b-aeb2-c151bb4b61db`
+  text = "hello"
+  `curl -X POST -d '{"text":"#{hello}"}' -H "Content-Type:application/json;charset=UTF-8" https://api.flock.co/hooks/sendMessage/5bb8d8db-0d55-436b-aeb2-c151bb4b61db`
 end
 
